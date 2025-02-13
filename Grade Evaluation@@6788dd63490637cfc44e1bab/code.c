a92eb7981516878df7634b801c00c04aa20e8cf3
heyyshivm1031 <shivamsharma87083@gmail.com>
2025-02-13 11:17:12 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -5,11 +5,10 @@ int main(){
     
     switch(ch){
         case A:printf("Excellent"); break;
-        case B:printf("Excellent"); break;
-        case C:printf("Excellent"); break;
-        case D:printf("Excellent"); break;
-        case E:printf("Excellent"); break;
-        case F:printf("Excellent"); break;
+        case B:printf("Good"); break;
+        case C:printf("Average"); break;
+        case D:printf("Below Average"); break;
+        case F:printf("Fail"); break;
     }
     return 0;
 }
