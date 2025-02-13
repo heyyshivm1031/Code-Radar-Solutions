be20a1600b3fdfd02772dcf1f22f1462f58619e6
heyyshivm1031 <shivamsharma87083@gmail.com>
2025-02-13 11:14:39 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    scanf("%c", &ch);
+    
+    switch(ch){
+        case A:printf("Excellent"); break;
+        case B:printf("Excellent"); break;
+        case C:printf("Excellent"); break;
+        case D:printf("Excellent"); break;
+        case E:printf("Excellent"); break;
+        case F:printf("Excellent"); break;
+    }
+    return 0;
+}
